5d7ecf3426ecb329b30e4af14238bd505732513d
yangbo1 <yangbo1@viewhigh.com>
2021-11-01 17:56:53 +0800

    陶特领取

---
+++ auto_20211111/test.js
@@ -0,0 +1,80 @@
+function start() {
+  auto.waitFor()
+  var appName = '钉钉'
+  if (launchApp(appName)) {
+    console.info('启动钉钉')
+  } else {
+    console.info('请手动启动淘宝APP')
+  }
+  sleep(4000)
+  id('session_list')
+    .findOne()
+    .children()
+    .forEach((child) => {
+      var target = child.findOne(
+        id('item_title').className('android.widget.TextView').text('打卡')
+      )
+      if (target) {
+        const bound = target.bounds()
+        click(bound.centerX(), bound.centerY())
+      } else {
+        log('未找到')
+      }
+    })
+  console.show()
+}
+execTask()
+// daka()
+function daka() {
+  sleep(4000)
+  // gesture(2000, [300, 800], [300, 1400])
+  // work_button = text('考勤打卡').findOnce()
+  work_button = className('android.widget.TextView').text('打卡').clickable()
+  if (work_button == null) {
+    log('找不到工作')
+    sleep(1000)
+  } else {
+    log('找到工作', work_button)
+    console.log('work_button.exists()===', work_button.exists())
+    const bound = work_button.findOne().bounds()
+    log('centerX', bound.centerX())
+    // click(bound.centerX(), bound.bottom - 20)
+    sleep(1000)
+  }
+  sleep(3000)
+  exit()
+}
+function execTask() {
+  console.show()
+  while (true) {
+    console.info('寻找按钮')
+    var target =
+      textStartsWith('逛一逛').findOnce() || textStartsWith('去浏览').findOnce()
+    if (target == null) {
+      console.info('任务完成')
+      toast('任务完成')
+      break
+    }
+    target.click()
+    console.info('去逛逛')
+    sleep(3000)
+    //浏览网页20s
+    viewWeb(15)
+    sleep(1000)
+  }
+  exit()
+}
+function viewWeb(time) {
+  gesture(1000, [300, 1000], [300, 800])
+  var cnt = 1
+  while (true) {
+    var finish = desc('任务完成').exists() || textStartsWith('已获得').exists()
+    if (finish || cnt > time) {
+      break
+    }
+    sleep(1000)
+    cnt += 1
+  }
+  //模拟返回键，返回到任务栏页面
+  back()
+}
---
+++ 陶特发财鸭/main.js
@@ -0,0 +1,89 @@
+/**
+ *
+ */
+
+function init() {
+  auto.waitFor()
+  console.show()
+  // start()
+  getCountReward()
+}
+// test()
+init()
+function test() {
+  auto.waitFor()
+  console.show()
+  console.log('test===')
+  var frame = text('浏览').findOnce()
+  if (frame) {
+    console.log('frame===', frame)
+    frame.click()
+    console.log('frame===', frame.bounds())
+    // click(483, 1500)
+  }
+  click(483, 1530)
+}
+// 启动淘特进入发财鸭页面
+function start() {
+  auto.waitFor()
+  var appName = '淘特'
+  if (launchApp(appName)) {
+    console.info('启动淘特')
+  } else {
+    console.info('请手动启动淘特APP')
+  }
+  sleep(4000)
+  className('android.support.v7.widget.RecyclerView')
+    .scrollable(true)
+    .depth(16)
+    .findOne()
+    .children()
+    .forEach((child) => {
+      var target = child.findOne(
+        className('android.widget.TextView').text('发财鸭')
+      )
+      if (target) {
+        const bound = target.bounds()
+        click(bound.centerX(), bound.centerY())
+      } else {
+        log('未找到')
+      }
+    })
+  console.show()
+}
+
+function getCountReward() {
+  log('getCountReward')
+  let flag = true
+  while (flag) {
+    console.log('找随机蛋...')
+    const btn = className('android.view.View').text('随机蛋').findOne()
+    if (btn) {
+      console.log('btn.bounds()===', btn.bounds())
+      btn = null
+      sleep(6000)
+      console.log('找到了随机蛋===')
+
+      click(414, 1150)
+      log('点击了随机蛋')
+      sleep(2000)
+      //点击普通领取
+      let getBtn = className('android.view.View').text('普通领取').findOnce()
+      log('getBtn', getBtn)
+      if (getBtn) {
+        log('普通领取')
+        getBtn.click()
+      } else {
+        //点击立即领取
+        log('立即领取')
+        click(483, 1500)
+      }
+    }
+    const end = className('android.view.View').text('明日再来').findOnce()
+    if (end) {
+      log('明日再来')
+      return false
+    }
+    sleep(3000)
+  }
+}
